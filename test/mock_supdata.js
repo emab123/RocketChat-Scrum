07dbb8e1e23ef3ed2af92479f16a1164f6d8479b
Daniel Schiffner <schiffner@dipf.de>
2022-04-28 13:38:48 +0200

    Fix: Updated mock functions

--- test/mock_supdata.js
+++ test/mock_supdata.js
@@ -8,12 +8,16 @@ if(process.env.DEBUG) {
     log.setLevel(log.levels.DEBUG);
 }
 
+sinon.stub(supData, "findNextResponseForUser");
 sinon.stub(supData, "findGroups");
 sinon.stub(supData, "store");
 sinon.stub(supData, "load");
 sinon.stub(supData, "updatePendingStatus");
 
 supData.reset = function() {
+    for(let m of ["findNextResponseForUser", "findGroups", "store", "load", "updatePendingStatus"]) {
+        supData[m].resetHistory();
+    }
     this.responses = [];
     this.groups = [];
     this.pending = [];
